b68fad3319bbcc2f6b52618801eb3bce80739c10
matiscava <matiscava@hotmail.com>
2023-08-22 15:08:51 -0300

    Modifique el corousel e hice responsive las flip cards

--- js/index.js
+++ js/index.js
@@ -1,41 +1,91 @@
-const carousel = document.getElementById('static-carousel-item-container');
+const $carousel = document.getElementById('static-carousel-item-container');
+const $btnCarouselLeft = document.getElementById('btn-static-carousel-left');
+const $btnCarouselRight = document.getElementById('btn-static-carousel-right');
+let $carouselSection = document.querySelectorAll(".static-carousel-item");
+let $carouselSectionLast = $carouselSection[$carouselSection.length-1];
+
+const $flipCardContainer = document.querySelector(".grid-flip-cards");
+const setFlipCardHeight = () => {
+  let width = window.innerWidth;
+  let rows = 0;
+  let height = 0;
+  if(width >= 992) {
+    rows = parseInt($flipCardContainer.children.length / 3);
+    if($flipCardContainer.children.length > rows*3) rows+=1;
+    height = rows * 70;
+  } else if( width < 992 && width > 767 ) {
+    rows = parseInt($flipCardContainer.children.length / 2);
+    if($flipCardContainer.children.length > rows*2) rows+=1;
+    height = rows * 70;
+  } else {
+    rows = $flipCardContainer.children.length
+    height = rows * 100;
+  }
+  console.log(`${rows * 80}vh`);
+  $flipCardContainer.style.height = `${height}vh`;
+}
+
+setFlipCardHeight();
+
 const intervalDuration = 3000; // Intervalo de tiempo en milisegundos
-let anchoCard  = carousel.children[0].clientWidth;
-let contador = 1;
+let anchoCard  = $carousel.children[0].clientWidth;
+let touchStartX = null;
+let intervalHandler;
+
+$carousel.insertAdjacentElement("afterbegin", $carouselSectionLast);
+
+
 window.addEventListener("resize", () => {
-  setWidth();
+  setCarouselWidth();
+  setFlipCardHeight();
 })
 
-function setWidth() {
-  let width = carousel.children.length * 100;
-  carousel.style.width = `${width}%`;
-}
+const moveCarouselRight = () => {
+  let $carouselSectionFirst = document.querySelectorAll(".static-carousel-item")[0];
+  $carousel.style.marginLeft = "-200%";
+  $carousel.style.transition = "all 1.5s";
+  setTimeout(() => {
+    $carousel.style.transition = "none";
+    $carousel.insertAdjacentElement("beforeend", $carouselSectionFirst);
+    $carousel.style.marginLeft = "-100%";
+  }, 1500);
+  clearInterval(intervalHandler);
+  intervalHandler = setInterval(moveCarouselRight, intervalDuration);
+} 
 
-function cloneFirstChild() {
-  const firstChild = carousel.children[0];
-  const newItem = firstChild.cloneNode(true);
-  carousel.appendChild(newItem);
-  setWidth();
-}
+const moveCarouselLeft = () => {
+  $carouselSection = document.querySelectorAll(".static-carousel-item");
+  $carouselSectionLast = $carouselSection[$carouselSection.length-1];
+  $carousel.style.marginLeft = "0%";
+  $carousel.style.transition = "all 1.5s";
+  setTimeout(() => {
+    $carousel.style.transition = "none";
+    $carousel.insertAdjacentElement("afterbegin", $carouselSectionLast);
+    $carousel.style.marginLeft = "-100%";
+  }, 1500);
+  clearInterval(intervalHandler);
+  intervalHandler = setInterval(moveCarouselRight, intervalDuration);
+} 
 
-function startSlideAnimation() {
-  const translateX = -contador * anchoCard;
-  carousel.style.transform = `translate(${translateX}px)`;
-  carousel.style.transition = 'transform 1s ease-in-out';
-  contador++;
+$btnCarouselRight.addEventListener('click', moveCarouselRight);
+$btnCarouselLeft.addEventListener('click', moveCarouselLeft);
+$carousel.addEventListener('touchstart', (e) => {
+  touchStartX = e.touches[0].clientX;
+})
 
-  if (contador >= carousel.children.length) {
-    setTimeout(() => {
-      carousel.style.transform = `translate(0px)`;
-      carousel.style.transition = "none";
-      contador = 1;
-      setTimeout(startSlideAnimation, intervalDuration);
-    }, 1000); // Tiempo de espera en milisegundos
-  } else {
-    setTimeout(startSlideAnimation, intervalDuration);
+$carousel.addEventListener('touchend', (e) => {
+  if(touchStartX !== null) {
+    const touchEndX = e.changedTouches[0].clientX;
+    const touchDifference = touchEndX - touchStartX;
+    touchDifference > 50 ? moveCarouselLeft() : moveCarouselRight();
+    touchStartX = null;
   }
+})
+
+const setCarouselWidth = () => {
+  let width = $carousel.children.length * 100;
+  $carousel.style.width = `${width}%`;
 }
 
-setWidth();
-cloneFirstChild();
-setTimeout(startSlideAnimation, intervalDuration);
+setCarouselWidth();
+intervalHandler = setInterval(moveCarouselRight, intervalDuration);
