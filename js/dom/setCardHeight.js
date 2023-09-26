8fa9d031476236f27c73e3ee178e732d0d4af994
matiscava <matiscava@hotmail.com>
2023-09-26 10:32:12 -0300

    Cambie algunas cosas en css para hacerlos andar en iphone

--- js/dom/setCardHeight.js
+++ js/dom/setCardHeight.js
@@ -1,9 +1,7 @@
 export default function setCardHeight () {
   const $cards = document.querySelectorAll('.height-240');
   const img = document.querySelector('.height-240 .img-container img');
-  console.log(img.clientHeight);
   $cards.forEach( $card => {
-    console.log($card);
     $card.style.height = `${img.clientHeight}px`;
   });
 }
