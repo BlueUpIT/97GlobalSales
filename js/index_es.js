4127f1acab8bd4bec7a63c443ae89872bed0bd3c
LautyFarias247 <lauty0618@gmail.com>
2023-10-02 19:50:45 -0300

    img telegram, titulos español

---
+++ js/index_es.js
@@ -0,0 +1,17 @@
+import heroTypingData from "./dom/heroTypingData_es.js";
+import setCardHeight from "./dom/setCardHeight.js";
+import setFlipCardHeight from "./dom/setFlipCardHeight.js";
+import spyScroll from "./dom/spyScroll.js";
+
+window.addEventListener("resize", () => {
+  setFlipCardHeight();
+  spyScroll();
+  setCardHeight();
+})
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  setFlipCardHeight();
+  heroTypingData();
+  spyScroll();
+  setCardHeight();
+})
